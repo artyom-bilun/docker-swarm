be4d5231a9b82b4edde8eb8982f028e4874ed364
Artyom Bilun <Artyom_Bilun@epam.com>
2017-06-06 19:58:47 +0300

    Added swarm features

--- provision/scripts/docker_run.sh
+++ provision/scripts/docker_run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
 
-docker import /vagrant/centos_7.tar centos:7
+docker load -i /vagrant/centos_7.tar
+
+
+docker build -f /vagrant/docker/httpd/Dockerfile -t mnt_httpd .
+docker service  create --name httpd -p 80:80 mnt_httpd
